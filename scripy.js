30a3d5962016faf4e90a2b73f6f98ca95d4b7b6d
yossefAlatter <yossefalatter@gmail.com>
2020-09-30 14:14:08 +0200

    playing-with-media

---
+++ scripy.js
@@ -0,0 +1,48 @@
+var share = document.querySelector(".share");
+var shareItems = document.querySelector(".share-items");
+var myTtrignal = document.querySelector(".mytrignal");
+var x = 4;
+var lgMedia = window.matchMedia("(min-width: 800px)");
+var smMedia = window.matchMedia("(max-width: 800px)");
+var tricker = document.getElementsByClassName("trick")[0];
+share.onclick = () => {
+  if (lgMedia.matches) {
+    if (x % 2 === 0) {
+      shareItems.style.display = "flex";
+      myTtrignal.style.display = "block";
+    } else {
+      shareItems.style.display = "none";
+      myTtrignal.style.display = "none";
+    }
+    x += 1;
+  }
+  if (smMedia.matches){
+    if (x % 2 === 0) {
+        shareItems.style.transform = "scaleX(1)";
+      } else {
+        shareItems.style.transform = "scaleX(0)";
+      }
+      x += 1;
+}
+};
+function mycheck (p) {
+    if (p.matches){
+        shareItems.style.display = "flex";
+        myTtrignal.style.display = "none";
+    }else{
+        shareItems.style.display = "none";
+        // myTtrignal.style.display = "none";
+    }
+}
+mycheck (smMedia) ;
+smMedia.addListener(mycheck);
+tricker.onclick= ()=>{
+    if (smMedia.matches){
+        if (x % 2 === 0) {
+            shareItems.style.transform = "scaleX(1)";
+          } else {
+            shareItems.style.transform = "scaleX(0)";
+          }
+          x += 1;
+    }
+}
